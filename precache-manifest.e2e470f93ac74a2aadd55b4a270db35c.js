b6c2f7d7f76480a8c358797173af6e600737de2c
Relise228 <andreysmit43@gmail.com>
2020-10-29 13:11:27 +0200

    Updates

---
+++ precache-manifest.e2e470f93ac74a2aadd55b4a270db35c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27dd27853dcd1591fad840e561d628f2",
+    "url": "/werdevs/index.html"
+  },
+  {
+    "revision": "030754b15bacbf72c3cc",
+    "url": "/werdevs/static/css/main.abc3de8b.chunk.css"
+  },
+  {
+    "revision": "08d29c39553363887d04",
+    "url": "/werdevs/static/js/2.0167fe79.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/werdevs/static/js/2.0167fe79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030754b15bacbf72c3cc",
+    "url": "/werdevs/static/js/main.501052f2.chunk.js"
+  },
+  {
+    "revision": "29099020ac37aad61b84",
+    "url": "/werdevs/static/js/runtime-main.70eb1463.js"
+  },
+  {
+    "revision": "fb5c09141c9348c972770a0039c9036f",
+    "url": "/werdevs/static/media/bg.fb5c0914.png"
+  },
+  {
+    "revision": "c1b2293586edb83bcd635cdb4e299109",
+    "url": "/werdevs/static/media/logo.c1b22935.jpg"
+  }
+]);
